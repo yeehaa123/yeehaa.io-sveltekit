ee6198ee2e3e180bee21c9fde4ad65e23b119918
Yeehaa <yeehaa@offcourse.io>
2023-08-13 22:44:17 +0200

    again...

--- svelte.config.js
+++ svelte.config.js
@@ -35,15 +35,7 @@ const config = {
 	extensions: ['.svelte', '.md'],
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
-	 adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false,
-      strict: true
-    })
+	 adapter: adapter()
 	}
 }
 
